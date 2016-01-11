57a5f03dd25cf0d92c43dca4cf368694a3786a75
cstoneburner <cstoneburner@tradecrafted.com>
2016-01-11 13:37:49 -0800

    added link to download resume

--- js/agency.js
+++ js/agency.js
@@ -24,3 +24,7 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+$('#download').click(function() {
+  $('#download').html('Thank You');
+});
